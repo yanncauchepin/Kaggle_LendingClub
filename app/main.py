89d6baa9186e2ceac6cac51de8b053011fdf6afc
yanncauchepin <cauchepin.yann@gmail.com>
2024-06-22 14:27:31 +0200

    progress

--- app/main.py
+++ app/main.py
@@ -465,8 +465,10 @@ def get_user_inputs():
     return input_
 
 import json
-def get_user_inputs_from_json(file_path=None):
-    if file_path is None:
+import os
+def get_user_inputs_from_json(file_path):
+    print(f"{os.getcwd()}")
+    if file_path is '':
         file_path = '/app/data_test.json'
         print("TEST JSON DATA")
     with open(file_path, 'r') as file:
